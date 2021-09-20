92ee1cae341dd09488479fb0baf3eb4c6144cdef
Stefanya <stefanya.aracely@gmail.com>
2021-09-20 11:03:39 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import cipher from './cipher.js';
 
-/*para cifrar*/ 
-const botonCifrar = document.getElementById("btn-cifrar");
-botonCifrar.addEventListener("click", cifrado);
-/*para descifrar*/ 
+/*para cifrar*/
+document.getElementById("btn-cifrar").addEventListener("click", cifrado);
+/*para descifrar*/
 const botonDescifrar = document.getElementById("btn-descifrar");
 botonDescifrar.addEventListener("click", descifrar);
- 
-function cifrado () {
+
+function cifrado() {
 	let desplazamiento = document.getElementById("desplazamiento").value;
 	let cifrarTexto = document.getElementById("texto").value;
 
 	if (cifrarTexto == "") {
 		alert("Ingrese contraseña a cifrar")
 	} else {
-			let mostrarTextoCifrado = cipher.encode(cifrarTexto, parseInt (desplazamiento));
-			document.getElementById("cifrad").value = mostrarTextoCifrado;
-		}
+		let mostrarTextoCifrado = cipher.encode(cifrarTexto, parseInt(desplazamiento));
+		document.getElementById("cifrad").value = mostrarTextoCifrado;
+	}
 }
 
-function descifrar (){
+function descifrar() {
 	let desplazamiento1 = document.getElementById("desplazamiento1").value;
 	let descifrarTexto = document.getElementById("texto1").value;
 
 	if (descifrarTexto == "") {
 		alert("Ingrese contraseña a descifrar")
 	} else {
-			let mostrarTextodescifrado = cipher.decode(descifrarTexto, parseInt (desplazamiento1));
-			document.getElementById("cifrad1").value = mostrarTextodescifrado;
+		let mostrarTextodescifrado = cipher.decode(descifrarTexto, parseInt(desplazamiento1));
+		document.getElementById("cifrad1").value = mostrarTextodescifrado;
 	}
 }
 
@@ -36,9 +35,9 @@ let btncifrar = document.querySelector(".pag1"); //boton cifrar
 btncifrar.addEventListener("click", nextpage);
 
 function nextpage() {
-document.querySelector(".pagprincipal").style.display = "none"; //none bloquea la pag
-document.querySelector(".pagcifrar").style.display = "block"; //block la muestra
-document.querySelector(".pagdescifrar").style.display = "none";
+	document.querySelector(".pagprincipal").style.display = "none"; //none bloquea la pag
+	document.querySelector(".pagcifrar").style.display = "block"; //block la muestra
+	document.querySelector(".pagdescifrar").style.display = "none";
 }
 
 let btndescifrar = document.querySelector(".pag2");
@@ -47,36 +46,36 @@ btndescifrar.addEventListener("click", nextpage1);
 function nextpage1() {
 	document.querySelector(".pagprincipal").style.display = "none"; //none bloquea la pag
 	document.querySelector(".pagcifrar").style.display = "none"; //block la muestra
-	document.querySelector(".pagdescifrar").style.display = "block";	
+	document.querySelector(".pagdescifrar").style.display = "block";
 }
 
 let btnvolver = document.querySelector(".buttonv .v1"); //btn volver
 btnvolver.addEventListener("click", back)
 
 function back() {
-	document.querySelector(".pagprincipal").style.display = "block"; 
+	document.querySelector(".pagprincipal").style.display = "block";
 	document.querySelector(".pagcifrar").style.display = "none";
-	document.querySelector(".pagdescifrar").style.display = "none";	
+	document.querySelector(".pagdescifrar").style.display = "none";
 }
 
 let btnvolver1 = document.querySelector(".buttonv .v2"); //btn volver
 btnvolver1.addEventListener("click", back1)
 
 function back1() {
-	document.querySelector(".pagprincipal").style.display = "block"; 
+	document.querySelector(".pagprincipal").style.display = "block";
 	document.querySelector(".pagcifrar").style.display = "none";
-	document.querySelector(".pagdescifrar").style.display = "none";	
+	document.querySelector(".pagdescifrar").style.display = "none";
 }
 
 let btnborrar = document.getElementById("borra"); //borrar texto de imputs
 btnborrar.addEventListener("click", borrar)
 
-function borrar() { 
+function borrar() {
 	document.getElementById("texto").value = "";
 	document.getElementById("cifrad").value = "";
 }
 
-let btnborrar1 = document.getElementById("borra1"); 
+let btnborrar1 = document.getElementById("borra1");
 btnborrar1.addEventListener("click", borrar1)
 
 function borrar1() {
@@ -87,7 +86,7 @@ function borrar1() {
 let copiar = document.getElementById("copy");
 copiar.addEventListener("click", copyToClipboard);
 
-function copyToClipboard() { 
+function copyToClipboard() {
 	let content = document.getElementById("cifrad");
 	content.select();
 	document.execCommand("copy");
@@ -98,7 +97,7 @@ function copyToClipboard() {
 let copiar1 = document.getElementById("copy1");
 copiar1.addEventListener("click", copyToClipboard1);
 
-function copyToClipboard1() { 
+function copyToClipboard1() {
 	let content = document.getElementById("cifrad1");
 	content.select();
 	document.execCommand("copy");
